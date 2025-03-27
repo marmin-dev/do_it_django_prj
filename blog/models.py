dcb7d0debd6b058a8f813b0996605f38bc4a7f00
Marmin <marmin.dev@gmail.com>
2025-03-27 16:09:22 +0900

    시간설정 및 추가

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    """
+    게시물
+    """
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+
+    # 처음 레코드가 생성될 때 현재 시간 추가
+    created_at = models.DateTimeField(auto_now_add=True)
+    # 레코드가 업데이트 될 때 현재 시간으로
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'[{self.pk}].{self.title}'
