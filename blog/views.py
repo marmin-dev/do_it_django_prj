97230677656e0bb2676d408e1542f11591393e56
Marmin <marmin.dev@gmail.com>
2025-03-28 16:49:11 +0900

    change blog.view FBV to CBV

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,44 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView
 from blog.models import Post
 
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts
-        }
-    )
+"""
+CBV
+"""
 
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post': post
-        }
-    )
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    # template_name = 'blog/index.html'
+
+
+class PostDetail(DetailView):
+    model = Post
+
+
+"""
+FBV
+"""
+
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#             'posts': posts
+#         }
+#     )
+#
+#
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+#     return render(
+#         request,
+#         'blog/single_post_page.html',
+#         {
+#             'post': post
+#         }
+#     )
