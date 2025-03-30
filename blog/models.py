8a87fc4afe213f2a6412ed9065fe40e2e2d0ee8d
Marmin <marmin.dev@gmail.com>
2025-03-30 20:51:17 +0900

    adding short header

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     게시물
     """
     title = models.CharField(max_length=30)
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
